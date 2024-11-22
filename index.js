ffef48d45c54100fba3e43755073edea3326c849
Kitae's Github <aqua0405@gmail.com>
2024-11-22 15:20:58 +0900

    기본 연동 테스트

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-const figlet = require("figlet");
-const express = require("express");
+const figlet = require('figlet');
+const express = require('express');
+const cors = require('cors');
 
-figlet("Node  Server", function (err, data) {
-    if (err) {
-        console.log("Something went wrong...");
-        console.dir(err);
-        return;
-    }
-    console.log(data);
+figlet('Node  Server', function (err, data) {
+	if (err) {
+		console.log('Something went wrong...');
+		console.dir(err);
+		return;
+	}
+	console.log(data);
 });
 
 const app = express();
+const port = 3000;
 
-app.get("/", function (req, res) {
-    res.send("Hello World");
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
 });
 
-app.listen(3000);
+app.get('/user/:id', (req, res) => {
+	const id = req.params.id;
+	res.json({ id: id });
+});
+
+app.get('/sound/:name', (req, res) => {
+	const {name} = req.params;
+	console.log(name);
+	if (name === 'cat') {
+		res.json({ sound: '야옹' });
+	} else if (name == 'dog') {
+		res.json({ sound: '멍멍' });
+	} else {
+		res.json({ sound: '알 수 없음' });
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Server Start - listening on port ${port}`);
+});
