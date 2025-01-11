843d6f31e0306b0accecd06f6af8621fd64458e3
Kitae's Github <aqua0405@gmail.com>
2025-01-12 01:36:49 +0900

    카트에 상품 추가

--- controllers/shop.js
+++ controllers/shop.js
@@ -62,21 +62,58 @@ exports.getCart = (req, res, next) => {
 		.catch((err) => console.log(err));
 };
 
+// 카트에 상품 추가
 exports.postCart = (req, res, next) => {
-	const prodId = req.body.productId; // 상품 ID를 가져옴
-	Product.findById(prodId, (product) => {
-		Cart.addProduct(prodId, product.price); // 카트에 상품 추가
-	});
-	res.redirect('/cart');
+	const prodId = req.body.productId; 		// 상품 ID를 가져옴
+	let fetchedCart;						// 카트를 가져옴
+	let newQuantity = 1;					// 상품 수량을 1로 설정
+
+	req.user
+		.getCart()
+		.then(cart => {						// 카트를 가져옴
+			fetchedCart = cart;				// 카트를 저장		
+			return cart.getProducts({ where: { id: prodId } });
+		})
+		.then(products => {						// 상품을 가져옴
+			let product;						// 상품을 저장할 변수
+			if (products.length > 0) {			// 상품이 이미 카트에 있는 경우
+				product = products[0];			// 첫 번째 상품을 가져옴					
+			}
+			
+			if(product) {						// 상품이 이미 있는 경우
+				const oldQuantity = product.cartItem.quantity;	// 기존 수량을 가져옴
+				newQuantity = oldQuantity + 1;	// 수량을 1 증가
+				return product;					// 상품을 반환
+			}
+			return Product.findByPk(prodId);	// 상품을 찾아 반환
+		})
+		.then(product => {
+			return fetchedCart.addProduct(product, {	// 카트에 상품을 추가
+				through: { quantity: newQuantity }		// 수량을 설정
+			});
+		})		
+		.then(() => {
+			res.redirect('/cart');				// 카트 페이지로 리다이렉트
+		})
+		.catch((err) => console.log(err));
 };
 
 // 카트에서 상품 삭제
 exports.postCartDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId; // 상품 ID를 가져옴
-	Product.findById(prodId, (product) => {
-		Cart.deleteProduct(prodId, product.price); // 카트에서 상품 삭제
-		res.redirect('/cart'); // 카트 페이지로 리다이렉트
-	});
+	req.user
+		.getCart()
+		.then(cart => {
+			return cart.getProducts({ where: { id: prodId } });	// 카트에서 상품을 찾아 반환
+		})
+		.then(products => {							// 상품을 가져옴
+			const product = products[0];			// 첫 번째 상품을 가져옴
+			return product.cartItem.destroy(); 		// 상품을 삭제
+		})
+		.then(() => {
+			res.redirect('/cart');					// 카트 페이지로 리다이렉트
+		})
+		.catch((err) => console.log(err))	
 };
 
 exports.getOrders = (req, res, next) => {
